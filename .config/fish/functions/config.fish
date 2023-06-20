d99f45ed7408e352425e6d6510a09786923e92b4
James Sharpe <james.sharpe@zenotech.com>
2023-06-20 20:55:18 +0100

    Add fish alias for config

---
+++ .config/fish/functions/config.fish
@@ -0,0 +1,4 @@
+function config --wraps='/usr/bin/git --git-dir=/Users/james.sharpe/dotfiles/ --work-dir=/Users/james.sharpe' --wraps='/usr/bin/git --git-dir=/Users/james.sharpe/dotfiles/ --work-tree=/Users/james.sharpe' --description 'alias config=/usr/bin/git --git-dir=/Users/james.sharpe/dotfiles/ --work-tree=/Users/james.sharpe'
+  /usr/bin/git --git-dir=/Users/james.sharpe/dotfiles/ --work-tree=/Users/james.sharpe $argv
+        
+end
